028b79a0895bdb072cbf704cd2fd1a5b5dc7c3eb
Jonathan Rajao <hei.tovomihaja@gmail.com>
2023-12-12 15:08:32 +0300

    feat:add method

--- src/main/java/com/company/base/endpoint/rest/controller/health/PingController.java
+++ src/main/java/com/company/base/endpoint/rest/controller/health/PingController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigInteger;
+
 @PojaGenerated
 @RestController
 @AllArgsConstructor
@@ -24,5 +26,11 @@ public class PingController {
   @GetMapping("/ping")
   public String ping() {
     return "pong";
+  }
+
+  @GetMapping("/new-prime")
+  public String getNewPrime() {
+    BigInteger probablePrime = BigInteger.probablePrime(10000, new java.util.Random());
+    return "Nouveau nombre premier probable : " + probablePrime.toString();
   }
 }
